6553d6924a8a53b17a36c8fcc047464b12ebeece
PREME Token Official <143048454+premetoken@users.noreply.github.com>
2024-04-21 18:24:44 +0200

    Update App.js

--- nft-minting-app/src/App.js
+++ nft-minting-app/src/App.js
@@ -162,8 +162,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > CONFIG.MAX_SUPPLY - data.totalSupply)
-      newMintAmount = CONFIG.MAX_SUPPLY - data.totalSupply;
+    if (newMintAmount > 30)
+      newMintAmount = 30;
     }
     setMintAmount(newMintAmount);
   };
